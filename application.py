e118f2ccd2ce953e487290ab8f0ffd685cc086bd
icottambbc <ian.cottam@bbc.co.uk>
2021-08-10 17:26:10 +0100

    Add some routes

--- application.py
+++ application.py
@@ -1,8 +1,15 @@
 from flask import Flask, render_template
 app = Flask(__name__)
+
 @app.route('/')
-def hello_world():
+def index():
   return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return "All about Flask"
+
+
 if __name__ == '__main__':
   app.run()
 
