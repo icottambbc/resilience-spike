b060e61bbe4dfc68a6f0828a839e5919f0b7ce57
icottambbc <ian.cottam@bbc.co.uk>
2021-08-10 17:16:33 +0100

    Serve an HTML page

--- application.py
+++ application.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
-  return 'Hello from Flask!'
+  return render_template("index.html")
 if __name__ == '__main__':
   app.run()
+
+# the plan
+# 1) Return a button
+# 2) When you click the button, it should run a program that generates an array with 1000 elements and then sums them. It returns the summed value
