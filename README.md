1c169d8d2d2cdfc6ad49ebc0c2ef54009e428afb
icottambbc <ian.cottam@bbc.co.uk>
2021-08-10 17:40:16 +0100

    Add a button to link to the calculation page

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 Use a basic service file (like you would with node) instead of apache
 
 - Use this: https://stackoverflow.com/questions/42004986/how-to-install-mod-wgsi-for-apache-2-4-with-python3-5-on-centos-7
+- A good flask guide: https://pythonise.com/series/learning-flask/flask-application-structure
--- application.py
+++ application.py
@@ -1,13 +1,18 @@
 from flask import Flask, render_template
+import numpy as np
 app = Flask(__name__)
+
+def sum():
+  DATA = [10, 9, 4, 15, 2, 9, 13, 10, 8, 8, 22, 6, 3, 8, 4, 12, 5, 9, 12]
+  return str(np.sum(DATA))
 
 @app.route('/')
 def index():
   return render_template("index.html")
 
-@app.route("/about")
+@app.route("/calculation")
 def about():
-    return "All about Flask"
+  return sum()
 
 
 if __name__ == '__main__':
@@ -15,4 +20,4 @@ if __name__ == '__main__':
 
 # the plan
 # 1) Return a button
-# 2) When you click the button, it should run a program that generates an array with 1000 elements and then sums them. It returns the summed value
+# 2) When you click the button, it should run a program that generates an array with 1000 elements and then sums them. It returns the summed value on a new page (need to pass the values to the new page)
